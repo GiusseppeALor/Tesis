a78c9a7c651daf2a5d48b7264c84f404f8bcdd88
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2025-01-16 15:53:54 -0500

    vid 1.0

---
+++ js/video.js
@@ -0,0 +1,4 @@
+// JavaScript para funcionalidades adicionales en la sección de Video de Recomendaciones
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Sección de video de recomendaciones cargada correctamente.");
+});
